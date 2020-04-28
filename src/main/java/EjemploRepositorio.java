39b1fa180a7d14e4434ac953ef6ad2b3889014a6
danie <danie@DESKTOP-O72JUUV>
2020-04-27 18:00:39 -0600

    Ejemplo

---
+++ src/main/java/EjemploRepositorio.java
@@ -0,0 +1,10 @@
+
+public class EjemploRepositorio {
+    
+    public static void main(String[] args){
+        
+        System.out.println("ESTO ES UN EJEMPLO DE REPOSITORIO ");
+        
+    }
+    
+}
